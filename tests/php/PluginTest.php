0bb351fa952a1ce82a36f9eeecc79e3c5b2da37a
Kaspars Dambis <hi@kaspars.net>
2019-08-16 14:44:48 +0300

    Few tests for the Plugin abstraction

---
+++ tests/php/PluginTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace XWP\BlockExtendTest;
+
+use WP_Mock;
+use XWP\BlockExtend\Plugin;
+
+/**
+ * Test the WordPress plugin abstraction.
+ */
+class PluginTest extends BlockExtendTestCase {
+
+	/**
+	 * Test the plugin setup.
+	 *
+	 * @covers XWP\BlockExtend\Plugin::__construct()
+	 * @covers XWP\BlockExtend\Plugin::file()
+	 * @covers XWP\BlockExtend\Plugin::dir()
+	 */
+	public function test_plugin_init() {
+		WP_Mock::userFunction( 'wp_upload_dir' )
+			->once()
+			->with( null, false );
+
+		$plugin = new Plugin( '/absolute/path/to/plugin.php' );
+
+		$this->assertEquals( '/absolute/path/to/plugin.php', $plugin->file() );
+		$this->assertEquals( '/absolute/path/to', $plugin->dir() );
+	}
+
+	/**
+	 * Test the plugin setup.
+	 *
+	 * @covers XWP\BlockExtend\Plugin::basename()
+	 */
+	public function test_basename() {
+		$mock = WP_Mock::userFunction( 'plugin_basename' )->twice();
+
+		$mock->with( '/any/random/file.js' )->andReturn( 'plugins/any/random/file.js' );
+
+		$plugin = new Plugin( '/path/to/plugin/file.php' );
+
+		$this->assertEquals(
+			'plugins/any/random/file.js',
+			$plugin->basename( '/any/random/file.js' ),
+			'Asks WP API to return the basename for a file'
+		);
+
+		$mock->with( '/path/to/plugin/file.php' )->andReturn( 'plugins/plugin/file.php' );
+
+		$this->assertEquals(
+			'plugins/plugin/file.php',
+			$plugin->basename(),
+			'Defaults to the main plugin file for the basename'
+		);
+	}
+
+}
