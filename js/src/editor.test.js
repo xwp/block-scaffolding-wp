312b4bf8444ecd2fa2d7584228f60ef1e106cca0
Kaspars Dambis <hi@kaspars.net>
2020-02-04 11:45:58 +0200

    Add a dummy function to test JS reporting

--- js/src/editor.test.js
+++ js/src/editor.test.js
@@ -1,6 +1,7 @@
+import { add } from './editor';
 
 describe( 'ensure setup works', () => {
 	it( 'should be equal when evaluating two arrays', () => {
-		expect( [] ).toStrictEqual( [] );
+		expect( add( 1, 2 ) ).toStrictEqual( 3 );
 	} );
 } );
